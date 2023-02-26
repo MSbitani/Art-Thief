09b80e7742411adc660a5e5f21c70b0eeab9af38
Marwan Sbitani <msbitani@gmail.com>
2023-02-25 21:10:44 -0500

    Update Functions Node version to 18

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,12 @@
 import * as functions from "firebase-functions";
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+let ssrServerServer: Function;
+exports.ssrServer = functions.region("us-central1").https.onRequest(async (request, response) => {
+  if (!ssrServerServer) {
+    functions.logger.info("Initialising SvelteKit SSR entry");
+    ssrServerServer = require("./ssrServer/index").default;
+    functions.logger.info("SvelteKit SSR entry initialised!");
+  }
+  functions.logger.info("Requested resource: " + request.originalUrl);
+  return ssrServerServer(request, response);
+});
--- public/_app/immutable/chunks/1.c3713427.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.a66816e8.js";export{t as component};
