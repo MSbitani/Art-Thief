e3268496c65825f84038dea799b5617397971f5f
Marwan Sbitani <msbitani@gmail.com>
2023-03-12 14:46:45 -0400

    Add theme switch component

---
+++ src/lib/ThemeSwitch.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { theme } from '$lib/stores';
+	import SegmentedButton, { Segment, Icon } from '@smui/segmented-button';
+
+	const themes = ['light', 'auto', 'dark'];
+</script>
+
+<SegmentedButton segments={themes} let:segment singleSelect bind:selected={$theme}>
+	<Segment {segment} title={`${segment.charAt(0).toUpperCase()}${segment.slice(1)} Theme`}>
+		<Icon class="material-symbols-outlined">{`${segment}_mode`}</Icon>
+	</Segment>
+</SegmentedButton>
