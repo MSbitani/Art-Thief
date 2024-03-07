040644833b86bea13168ef39fcfa4dfea5211dde
msbitani <msbitani@gmail.com>
2024-03-07 11:14:57 +0000

    Update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,5 @@
-import firebase from "svelte-adapter-firebase";
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: firebase()
-	}
+export default {
+	preprocess: [vitePreprocess()]
 };
-
-export default config;
